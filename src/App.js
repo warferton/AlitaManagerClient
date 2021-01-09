4e8e99cd3c4f1ae1fe6c8e8cdef5c28503c70b75
warferton <warferton45@gmail.com>
2021-01-09 15:37:35 +0500

    sdasd

--- src/App.js
+++ src/App.js
@@ -22,13 +22,13 @@ const HomePage = lazy(() => import('./components/layouts/HomePage'))
 // axios.defaults.headers(['X-CSRF-Token'])
 
 //only send jwt to the api
-const apiUrl = "https://fast-sierra-37663.herokuapp.com/api/"//"http://localhost:3000/api/"
+const apiUrl = "http://localhost:8080/api/"//"https://fast-sierra-37663.herokuapp.com/api/"//"http://localhost:3000/api/"
 
 axios.defaults.headers.common["Access-Control-Allow-Origin"] = "*"
 
 axios.interceptors.request.use(config =>{
   const {origin} = new URL(config.url)
-  const allowedOrigins = ['https://fast-sierra-37663.herokuapp.com']
+  const allowedOrigins = ['http://localhost:8080']
   if(!allowedOrigins.includes(origin)){
     config.headers.Authorization = null
     return config
